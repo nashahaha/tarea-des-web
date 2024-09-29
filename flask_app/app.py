f8524239ac177bee02ff2adabd2fd305463199e7
Nasha <ignacia.galaz@ug.uchile.cl>
2024-09-29 00:07:05 -0300

    feat(app)!: en proceso de establecer la navegacion por las páginas

---
+++ flask_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# ----- pagina principal -----
+@app.route("/")
+def index():
+    return render_template("html/index.html")
+
+@app.route("/agregar-donacion")
+def agregarDonacion():
+    return render_template("html/agregar-donacion.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
