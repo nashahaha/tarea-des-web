8e1a14f9caf955325a3ebb7a4ba81eb2da08764f
Nasha <ignacia.galaz@ug.uchile.cl>
2024-10-01 01:35:58 -0300

    fix(app, requirements): arreglé los tipos que se insertan el la db y agregué un requirement

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from utils.validations import validate_user, validate_device
 from database import db
-import datetime
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -26,8 +26,8 @@ def verDispositivos():
 def confirmarForm():
     name   = request.form.get["username"]
     email  = request.form.get["email"]
-    phone  = request.form.get["phone-number"]
-    region = request.form.get["select-region"]
+    phone  = request.form.get["phone-number"] # int?, no es necesario?
+    region = request.form.get["select-region"] 
     comuna = request.form.get["select-comuna"]
 
     device  = request.form.get["device-name"]
@@ -37,10 +37,12 @@ def confirmarForm():
     estado  = request.form.get["working-status"]
     fotos   = request.files.get["device-pics"]
 
+    print(name, email)
+
     if validate_user(name, email, phone, region, comuna):
         ## aquí debería buscar la comuna entre la base de datos y retornar el id
-        comuna_id = db.get_id_comuna_by_nombre()
-        fecha_creacion = datetime.datetime.now()
+        comuna_id = db.get_id_comuna_by_nombre(comuna) # int
+        fecha_creacion = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
         db.create_user(name, email, phone, comuna_id, fecha_creacion)
         
     
