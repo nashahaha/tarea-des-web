638bfa63e4125e0eb6362baae8d1197790fdb7e4
Nasha <ignacia.galaz@ug.uchile.cl>
2024-10-01 01:25:43 -0300

    fix(db): debugged up until the functions that call the database

--- flask_app/database/db.py
+++ flask_app/database/db.py
@@ -1,14 +1,15 @@
 import pymysql
 import json
+import os
 
 DB_NAME = "tarea2"
 DB_USERNAME = "cc5002"
-DB_PASSWORD = "programacionweb"
+DB_PASSWORD = "NeoG352912"
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_CHARSET = "utf8"
 
-with open('database/querys.json', 'r') as querys:
+with open('querys.json', 'r', encoding="UTF-8") as querys:
 	QUERY_DICT = json.load(querys)
 
 # -- conn ---
@@ -36,3 +37,6 @@ def get_id_comuna_by_nombre(nombre_comuna):
 	cursor.execute(QUERY_DICT["get_comuna_by_nombre"], (nombre_comuna,))
 	comuna = cursor.fetchone()
 	return comuna
+
+get_id_comuna_by_nombre("Independecia")
+create_user("Juan Carlos Bodoque", "juan.carlos@31minutos.cl", "012345678", 130216, "2024-09-31 23:59:07")
