a87e4741bd43433569f5d10a6a196842ff573480
Nasha <ignacia.galaz@ug.uchile.cl>
2024-10-01 01:30:45 -0300

    fix(validations): fixed validation conditions

--- flask_app/utils/validations.py
+++ flask_app/utils/validations.py
@@ -3,24 +3,24 @@ import filetype
 
 def validate_name(name):
     valid_name = re.compile(r"^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$")
-    return name and len(name)>=3 and len(name)<=80 and valid_name.match(name)
+    return name!=None and len(name)>=3 and len(name)<=80 and valid_name.match(name)
 
 def validate_email(email):
     valid_email = re.compile(r"^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$")
-    return email and valid_email.match(email) and (email.count("@")==1)
+    return email!=None and valid_email.match(email) and (email.count("@")==1)
 
 def validate_phone(phone):
     valid_phone = re.compile(r"^(\+?56)?(\s?)(0?9)(\s?)[98765432]\d{7}$")
     return valid_phone.match(phone)
 
 def validate_device_name(device):
-    return device and len(device)>=3 and len(device)<=80
+    return device!=None and len(device)>=3 and len(device)<=80
 
 def validate_description(description):
     return description<=500
 
 def validate_years(years):
-    return years and years>=1 and years<=99 and type(years)==int
+    return years!=None and years>=1 and years<=99 and type(years)==int
 
 def validate_file(file):
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
